f08d306771a2487bdd814e74beb264db219106e5
geumkyeong <geumkyeong@github.com>
2018-12-17 23:26:03 +0900

    edit 05

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@
   var $newNote = $('.btn-newnote');
   var $saveNote = $('.btn-savenote');
   var $makeNote = $('.btn-makenote');
+  var $fsOn = $('.fs-on');
+  // var $fsOff = $('.fs-off');
   var $about = $('.about');
 
   $newNote.on('click', app.view.newNote );
@@ -24,13 +26,16 @@
       save += `id: ${initData[i].id}, `;
       save += `content: ${initData[i].content} `;
     }
-    console.log(save);
     var blob = new Blob([save], {
       type: "text/plain;charset=utf-8"
     });
     saveAs(blob, "sample-file.txt");
   });
 
+
+  $about.on('click', function(event){
+    alert("2016108265 문금경");
+  });
 
   // //삭제버튼 이벤트 잡기 위해 상위에서 이벤트 listen 하기
   $listDom.on('click', '.delete', function(event){
@@ -46,12 +51,33 @@
 
   // //처음 로딩시에 기존에 저장된 데이터 가져와서 보여주기
   var initData = app.util.storage.load();
+
+  var save = '';
+  for(var i=0; i < initData.length; i++){
+    console.log(initData[i]);
+    save += `id: ${initData[i].id}, `;
+    save += `content: ${initData[i].content}`;
+  }
+
   if(initData) {
     console.log(initData)
-    
+    $memoField.val(save);
     app.collection.set(initData);
   }
 
+  //Full Screen ON
+  var elem = $(document.body);
 
+  $fsOn.on('click', function() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  }); 
 
 })(jQuery, window, Note);
